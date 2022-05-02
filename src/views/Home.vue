269628df62cf0e280f517862df0f90654e1b557a
Rodrigo P <rportugal@cualit.com>
2022-05-01 22:41:18 -0300

    Final Project for the course 😀

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    <px-assets-table v-if="!isLoading" :assets="assets" />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable.vue'
+import api from '@/api'
+
+export default {
+  name: '',
+  components: {
+    PxAssetsTable,
+  },
+  data() {
+    return {
+      assets: [],
+      isLoading: true,
+    }
+  },
+  created() {
+    api
+      .getAssets()
+      .then((response) => (this.assets = response))
+      .finally(() => (this.isLoading = false))
+  },
+}
+</script>
